f4b133627d61656b49fddecbf32b1502397c4faa
Nicko229 <you@example.com>
2019-04-23 12:41:41 +0200

    list is responsive and on same line

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Banner from './Banner';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      images: []
+      books: []
     }
   }
 
@@ -14,23 +15,34 @@ class App extends Component {
     axios.get('http://localhost:4000/products')
       .then(res => {
         // console.log(res.data);
-        this.setState({ images: res.data });
-        console.log("state: ", this.state)
-        res.json();
+        this.setState({ books: res.data });
+        console.log(res.data)
+        // res.json();
       })
       .catch(error => {
         console.log(error)
       })
   }
 
+
+  // create an onClick function that saves the image id when it is clicked.
+
+
   render() {
-    const image = this.state.images.map(val => {
+    const image = this.state.books.map(val => {
       return (
-        <p>{val.image}</p>
+        <div className="image-div">
+          <a href="www.google.com">
+            <img className="images" src={val.image} />
+            <p>{val.title}</p>
+            <p>{val.author}</p>
+          </a>
+        </div>
       )
     })
     return (
       <div className="App">
+        <Banner />
         {image}
       </div>
     );
