2a70003a7fb8a7a8d71a112cd39239cc3fcd587b
Nicko229 <you@example.com>
2019-04-23 10:21:33 +0200

    images urls coming through to react page

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -3,11 +3,19 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      images: []
+    }
+  }
 
   componentDidMount = () => {
     axios.get('http://localhost:4000/products')
       .then(res => {
-        console.log(res.data);
+        // console.log(res.data);
+        this.setState({ images: res.data });
+        console.log("state: ", this.state)
         res.json();
       })
       .catch(error => {
@@ -16,9 +24,14 @@ class App extends Component {
   }
 
   render() {
+    const image = this.state.images.map(val => {
+      return (
+        <p>{val.image}</p>
+      )
+    })
     return (
       <div className="App">
-
+        {image}
       </div>
     );
   }
